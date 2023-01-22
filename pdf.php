eb679ae13b22b5814d10f7e3368d07313cd414b7
slabs37 <42916693+slabs37@users.noreply.github.com>
2023-01-22 13:30:35 +0330

    Added loading animation

--- pdf.php
+++ pdf.php
@@ -1,3 +1,28 @@
+<html>
+    
+<style>
+    .text {
+        text-align: center;
+        font-size: 1.2em;
+    }
+
+    .loader {
+      border: 2vh solid #f3f3f3; /* Light grey */
+      border-top: 2vh solid #206ba4;
+      border-radius: 50%;
+      width: 25vh;
+      height: 25vh;
+      margin: auto;
+      margin-top:10%;
+     animation: spin 2s linear infinite;
+    }
+    
+    @keyframes spin {
+      0% { transform: rotate(0deg); }
+      100% { transform: rotate(360deg); }
+    }
+</style>
+
 <?php
 if (isset($_GET['page'])) {
     $page = $_GET['page'];
@@ -26,29 +51,37 @@ $pagae = $in_var-1;
     
     $newurl = "pdf.php".'?file='.$file.'&name='.$name.'&page='.$pagae.'';
     
-    echo '<script>window.location.href="'.$newurl.'"</script>';
+    echo '
+    <div class="text">Converting page '.$in_var.' to image... </div><br>
+    <div class="loader"></div>
+    <script>window.location.href="'.$newurl.'"</script>';
     
     }
-
-
 } else {
     die();
 }
 
 ?>
-<html>
+
+    
     <img id="imga" src=<?php //show images with base64 data so that we can delete the temp files quickly
     $FilePath = $name.".jpg"; 
     $data = base64_encode(file_get_contents($FilePath));
     echo '"data:image/jpg;base64,' . $data . '"'; 
     unlink($FilePath);
     ?> style="overflow-x: hidden; display: block; margin-left: auto; margin-right: auto;"> </img> 
-    <div>
-	    <button id="go_previous" style="position: fixed;  top: 95%;  left: 32%;  transform: translate(-50%, -50%); background-color: #206ba4; border-radius: 50px; color: white;">&#x25c0; prev</button>
+    
+    <div id="loading" style="display:none">
+        <div class="text">Converting page <span id="pagg"></span> to image... </div><br>
+        <div class="loader"></div>
+    </div>
+    
+    <div style="margin: 25px">
+	    <button id="go_previous" style="position: fixed; bottom: 0px; left: 32%; transform: translate(-50%, 0%); background-color: #206ba4; border-radius: 50px; color: white;">&#x25c0; prev</button>
 	    
-	    <input style="-webkit-appearance: none; margin: 0; -moz-appearance:textfield; position: fixed;  top: 95%;  left: 50%;  transform: translate(-50%, -50%); background-color: #206ba4; border-radius: 10px; color: white; width: 3em; text-align: center;" onkeydown="goPage(this)" value="<?php echo $_GET['page']+1 ?>" >
+	    <input style="-webkit-appearance: none; margin: 0; -moz-appearance:textfield; position: fixed; bottom: 0px; left: 50%; transform: translate(-50%, 0%); background-color: #206ba4; border-radius: 10px; color: white; width: 3em; text-align: center;" onkeydown="goPage(this)" value="<?php echo $_GET['page']+1 ?>" >
 
-	    <button id="go_next" style="position: fixed;  top: 95%;  left: 68%;  transform: translate(-50%, -50%); background-color: #206ba4; border-radius: 50px; color: white;">next &#x25ba;</button>
+	    <button id="go_next" style="position: fixed; bottom: 0px; left: 68%; transform: translate(-50%, 0%); background-color: #206ba4; border-radius: 50px; color: white;">next &#x25ba;</button>
         </div> 
     
         
@@ -70,12 +103,14 @@ var page = getUrlVars()["page"];
     document.getElementById('go_previous').addEventListener('click', (e) => {
         if (page==0) return;
             page -= 1;
+            bringLoad(page);
             window.location.href = "pdf.php?file="+file+"&name="+name+"&page="+page;
     });
  
     document.getElementById('go_next').addEventListener('click', (e) => {
             
             page++;
+            bringLoad(page);
             window.location.href = "pdf.php?file="+file+"&name="+name+"&page="+page;
             
     });
@@ -84,9 +119,17 @@ var page = getUrlVars()["page"];
     if(event.key === 'Enter') {
         var inputPage = ele.value-1
 	if (inputPage < 1) {inputPage = 0}
+	    bringLoad(inputPage);
         window.location.href = "pdf.php?file="+file+"&name="+name+"&page="+inputPage;        
     }
 }
+    let width = screen.width/1.25;
+    document.getElementById('imga').style.width = width+"px";
     
+function bringLoad(pagg) {
+document.getElementById('pagg').innerHTML=pagg+1;
+document.getElementById('imga').style="display:none";
+document.getElementById('loading').style="";
+}
 </script>
 </html>
